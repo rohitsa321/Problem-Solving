995f126b0ad61cf3b8eeff6719675d09be977a93
rohitsa321 <52958581+rohitsa321@users.noreply.github.com>
2020-03-30 16:54:47 +0530

    Update and rename nptelq2 to Nptel_Q2.java

--- Nptel_Q2.java
+++ Nptel_Q2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;  
-public class Exercise1_2 {
+public class Nptel_Q2 {
        public static void main(String[] args) {
         Scanner s = new Scanner(System.in); 
         int x = s.nextInt(); 
