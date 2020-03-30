43c38af3afd008242aee148ecdaf68e61563f738
rohitsa321 <52958581+rohitsa321@users.noreply.github.com>
2020-03-30 16:53:07 +0530

    Update and rename nptelq1 to Nptel_Q1.java

--- Nptel_Q1.java
+++ Nptel_Q1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;  
-public class Exercise1_1 {
+public class Nptel_Q1 {
        public static void main(String[] args) {
        Scanner s = new Scanner(System.in); 
        double radius= s.nextDouble();
